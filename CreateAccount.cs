501ca56fdbf7c1896525dc60479e0a37bd1cedf4
Btenks <btenks95@gmail.com>
2024-04-19 00:30:38 +0200

    TTHE CHANGES

--- CreateAccount.cs
+++ CreateAccount.cs
@@ -35,51 +35,8 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (connect.State != ConnectionState.Open)
-            {
-                try
-                {
-                    connect.Open();
-                    string checkUsername = "SELECT * FROM admin WHERE username = '"
-                        + signup_username.Text.Trim() + "'";
-
-                    using (SqlCommand checkUser = new SqlCommand(checkUsername, connect))
-                    {
-                        SqlDataAdapter adapter = new SqlDataAdapter(checkUser);
-                        DataTable table = new DataTable();
-                        adapter.Fill(table);
-
-                        if (table.Rows.Count >= 1)
-                        {
-                            MessageBox.Show(signup_username.Text + " already exists", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-                        else
-                        {
-                            string insertData = "INSERT INTO admin (email, username, pasword, date_created) "
-                                "VALUES(@email,@username,@pass,@date)";
-                            DateTime date = DateTime.Today;
-
-                            using SqlCommand cmd = new SqlCommand(insertData, connect)) ;
-                            {
-                                cmd.Parameters.AddWithValue("@email", signup_hitmail.Text.Trim());
-                                cmd.Parameters.AddWithValue("@pass", signup_hitmail.Text.Trim());
-                                cmd.Parameters.AddWithValue("@username", signup_hitmail.Text.Trim());
-                                cmd.Parameters.AddWithValue("@date", date);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error connecting Database" + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                finally
-                {
-                    connect.Close();
-                }
-            }
+            
         }
-
         private void signup_hitmali_TextChanged(object sender, EventArgs e)
         {
 
